54487f8d3e9e7b2cf38e3a3a4146f23d7df38aee
Varun Borar <varunborar@gmail.com>
2022-04-19 00:45:02 +0530

    ready for deployment

--- SRParser/urls.py
+++ SRParser/urls.py
@@ -22,6 +22,6 @@ from django.views.static import serve
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('srparsersite/', include('srparsersite.urls')),
-    url(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}),
-    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
+    # url(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}),
+    # url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
 ]
